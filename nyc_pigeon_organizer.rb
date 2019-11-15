20df8890ef4afec2e06be7381fbce86ce0f52f37
Jacy Leech <jacy.atlanta@gmail.com>
2019-11-15 17:15:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,5 +2,14 @@ def nyc_pigeon_organizer(data)
   new_pidg_data = {}
   
   data.each do |key_pidg, val_pidg|
-    val_pidg.each do |key_pidg2, val_pidg2 |
+    val_pidg.each do |key_pidg2, val_pidg2|
+      val_pidg2.each do |name|
+      new_pidg_data[name]  = {
+        color: [name][:color],
+        gender: [name][:gender]
+        lives: [name][:lives]
+      }
+end
+end
+end
 end
